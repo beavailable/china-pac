05b4fc6c9379e688810e892b49c5ffcdc59864d7
beavailable <beavailable@proton.me>
2025-12-02 12:31:20 +0800

    Fix the generate.sh

--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ shopt -s inherit_errexit
 {
     echo -n "const domains = new Set('"
     echo -n "$(sed -nE '/^[^#]/p' domains.list extra.list | paste -sd ' ')"
-    echo "'.split(' ');"
+    echo "'.split(' '));"
     tail +2 proxy.pac.template | sed -E "s/TEMPLATE_PROXY/$(cat proxy)/"
 } >proxy.pac
